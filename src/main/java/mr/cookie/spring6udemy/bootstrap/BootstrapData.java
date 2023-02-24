1e91aced49fa3468513e780c7244dd0634d61454
Davé <cookieMr@users.noreply.github.com>
2023-02-24 20:04:27 +0100

    Bootstrap publisher + tests (#9)
    * Bootstrap publisher + tests
    
    * User Argument Captor for Tests

--- src/main/java/mr/cookie/spring6udemy/bootstrap/BootstrapData.java
+++ src/main/java/mr/cookie/spring6udemy/bootstrap/BootstrapData.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import mr.cookie.spring6udemy.domain.Publisher;
 import mr.cookie.spring6udemy.repositories.PublisherRepository;
+import org.apache.commons.collections4.IterableUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -33,39 +34,6 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        var bSandersonAuthor = Author.builder()
-                .firstName("Brandon")
-                .lastName("Sanderson")
-                .build();
-
-        var saBook1 = Book.builder()
-                .title("Way of Kings")
-                .isbn("978-0765365279")
-                .build();
-        var saBook2 = Book.builder()
-                .title("Words of Radiance")
-                .isbn("978-0765326362")
-                .build();
-        var saBook3 = Book.builder()
-                .title("Oathbringer")
-                .isbn("978-0765326379")
-                .build();
-        var saBook4 = Book.builder()
-                .title("Rhythm of War")
-                .isbn("978-0765326386")
-                .build();
-
-        var savedSandersonAuthor = this.authorRepository.save(bSandersonAuthor);
-        var savedSaBook1 = this.bookRepository.save(saBook1);
-        var savedSaBook2 = this.bookRepository.save(saBook2);
-        var savedSaBook3 = this.bookRepository.save(saBook3);
-        var savedSaBook4 = this.bookRepository.save(saBook4);
-
-        savedSandersonAuthor.getBooks().addAll(Arrays.asList(
-            savedSaBook1, savedSaBook2, savedSaBook3, savedSaBook4
-        ));
-        this.authorRepository.save(savedSandersonAuthor);
-
         var dragonsteelPublisher = Publisher.builder()
                 .name("DragonSteel Books")
                 .address("POBox 698")
@@ -73,7 +41,38 @@ public class BootstrapData implements CommandLineRunner {
                 .city("American Fork")
                 .zipCode("84003")
                 .build();
-        this.publisherRepository.save(dragonsteelPublisher);
+        var savedPublisher = this.publisherRepository.save(dragonsteelPublisher);
+
+        var saBook1 = Book.builder()
+                .title("Way of Kings")
+                .isbn("978-0765365279")
+                .publisher(savedPublisher)
+                .build();
+        var saBook2 = Book.builder()
+                .title("Words of Radiance")
+                .isbn("978-0765326362")
+                .publisher(savedPublisher)
+                .build();
+        var saBook3 = Book.builder()
+                .title("Oathbringer")
+                .isbn("978-0765326379")
+                .publisher(savedPublisher)
+                .build();
+        var saBook4 = Book.builder()
+                .title("Rhythm of War")
+                .isbn("978-0765326386")
+                .publisher(savedPublisher)
+                .build();
+        var savedBooks = IterableUtils.toList(this.bookRepository.saveAll(
+                Arrays.asList(saBook1, saBook2, saBook3, saBook4)
+        ));
+
+        var bSandersonAuthor = Author.builder()
+                .firstName("Brandon")
+                .lastName("Sanderson")
+                .build();
+        bSandersonAuthor.getBooks().addAll(savedBooks);
+        this.authorRepository.save(bSandersonAuthor);
 
         this.printRepositoriesCounts();
     }
--- src/test/java/mr/cookie/spring6udemy/units/bootstrap/BootstrapDataTest.java
+++ src/test/java/mr/cookie/spring6udemy/units/bootstrap/BootstrapDataTest.java
@@ -7,15 +7,20 @@ import mr.cookie.spring6udemy.domain.Publisher;
 import mr.cookie.spring6udemy.repositories.AuthorRepository;
 import mr.cookie.spring6udemy.repositories.BookRepository;
 import mr.cookie.spring6udemy.repositories.PublisherRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -35,31 +40,38 @@ class BootstrapDataTest {
     @InjectMocks
     private BootstrapData bootstrapData;
 
+    @Captor
+    private ArgumentCaptor<List<Book>> booksCaptor;
+
     @Test
     void run() {
+        var savedPublisher = Publisher.builder().id(111L).build();
         var savedAuthor = Author.builder().id(42L).build();
         var savedBook1 = Book.builder().id(1L).build();
         var savedBook2 = Book.builder().id(2L).build();
         var savedBook3 = Book.builder().id(3L).build();
         var savedBook4 = Book.builder().id(4L).build();
 
+        when(this.publisherRepository.save(any(Publisher.class)))
+                .thenReturn(savedPublisher);
+        when(this.bookRepository.saveAll(this.booksCaptor.capture()))
+                .thenReturn(Arrays.asList(savedBook1, savedBook2, savedBook3, savedBook4));
         when(this.authorRepository.save(any(Author.class)))
                 .thenReturn(savedAuthor);
-        when(this.bookRepository.save(any(Book.class)))
-                .thenReturn(savedBook1, savedBook2, savedBook3, savedBook4);
 
         this.bootstrapData.run();
 
-        Assertions.assertThat(savedAuthor.getBooks())
-                .containsExactlyInAnyOrder(savedBook1, savedBook2, savedBook3, savedBook4);
-
-        verify(this.authorRepository, times(2)).save(any(Author.class));
+        verify(this.authorRepository).save(any(Author.class));
         verify(this.authorRepository).count();
-        verify(this.bookRepository, times(4)).save(any(Book.class));
+        verify(this.bookRepository).saveAll(anyList());
         verify(this.bookRepository).count();
         verify(this.publisherRepository).save(any(Publisher.class));
         verify(this.publisherRepository).count();
         verifyNoMoreInteractions(this.authorRepository, this.bookRepository, this.publisherRepository);
+
+        assertThat(this.booksCaptor.getValue())
+                .isNotEmpty()
+                .allMatch(book -> book.getPublisher().equals(savedPublisher));
     }
 
 }
