6c483eb43d7a511e77163aceae2a144f3c62d23c
Davé <cookieMr@users.noreply.github.com>
2023-02-24 23:28:00 +0100

    Service Tests for Controller Endpoint for Books (#13)
    * Service Tests for Controller Endpoint for Books
    
    * Fix Sonar Issue

---
+++ src/test/java/mr/cookie/spring6udemy/services/BookControllerTest.java
@@ -0,0 +1,31 @@
+package mr.cookie.spring6udemy.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllBooks() throws Exception {
+        this.mockMvc.perform(get("/books"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+    }
+
+}
