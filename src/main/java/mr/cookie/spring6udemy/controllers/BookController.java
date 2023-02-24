6cec6dfc335dba19f3d04a90e6dd10b466b66403
cookieMr <dawid.boisse@gmail.com>
2023-02-24 23:21:29 +0100

    Service Tests for Controller Endpoint for Books

--- src/main/java/mr/cookie/spring6udemy/controllers/BookController.java
+++ src/main/java/mr/cookie/spring6udemy/controllers/BookController.java
@@ -3,6 +3,7 @@ package mr.cookie.spring6udemy.controllers;
 import lombok.RequiredArgsConstructor;
 import mr.cookie.spring6udemy.model.model.Book;
 import mr.cookie.spring6udemy.services.BookService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Book> getBooks() {
         return this.bookService.findAll();
     }
