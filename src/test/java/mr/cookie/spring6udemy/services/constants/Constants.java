589f7435ca9bd75c259f322260c968eb2bfbc022
cookieMr <dawid.boisse@gmail.com>
2023-03-09 21:01:01 +0100

    Service Tests for Validation (Author + Book)

---
+++ src/test/java/mr/cookie/spring6udemy/services/constants/Constants.java
@@ -0,0 +1,10 @@
+package mr.cookie.spring6udemy.services.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final String BLANK_STRING = "";
+
+}
