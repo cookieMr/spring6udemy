070d8226271118507225de3a58d6153ab095196f
Davé <cookieMr@users.noreply.github.com>
2023-11-14 21:12:28 +0100

    Clean Up JaCoCo Setup (#64)

--- src/integrationTest/java/mr/cookie/spring6udemy/utils/MvcResponseWithBookContent.java
+++
@@ -1,9 +0,0 @@
-package mr.cookie.spring6udemy.utils;
-
-import java.util.List;
-import mr.cookie.spring6udemy.model.dtos.BookDto;
-
-public record MvcResponseWithBookContent(
-        List<BookDto> content
-) {
-}
